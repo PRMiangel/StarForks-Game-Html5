6d26cdb0f23a6bd8d4ddea079609ea1bc903afb5
Daniel Barreto <dbarreto@talpor.com>
2012-11-04 14:29:01 -0430

    the StarsField now has two fields that will rotate to simulate an infinite space.

--- static/js/modules/stars.js
+++ static/js/modules/stars.js
@@ -1,12 +1,22 @@
 define(['gamejs', 'modules/globals', 'modules/utils'], function(gamejs, globals, utils) {
     var StarsField = function() {
         var size  = utils.hypotenuse(globals.game.screenSize);
-        this.surface = new utils.PatternSurface([size, size]);
-        this.surface.fillPattern(globals.starsField.image);
+
+        this.field1 = new utils.PatternSurface([size, size]);
+        this.field2 = new utils.PatternSurface([size, size]);
+
+        this.field1.fillPattern(globals.starsField.image);
+        this.field2.fillPattern(globals.starsField.image);
+
+        this.surface = new gamejs.Surface([size, size*2]);
+        this.surface.blit(this.field1, [0, 0]);
+        this.surface.blit(this.field1, [0, size]);
+
+        this.offset = [0, 0];
     };
 
-    StarsField.prototype.draw = function(display, offset) {
-        display.blit(this.surface, offset);
+    StarsField.prototype.draw = function(display) {
+        display.blit(this.surface, this.offset);
     };
 
     return {
