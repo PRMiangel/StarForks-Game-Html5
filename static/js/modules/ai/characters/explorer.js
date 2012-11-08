0db66f20380d7a9e305039a2f4b6789629c7a6ed
Daniel Barreto <dbarreto@talpor.com>
2012-11-07 20:20:11 -0430

    adding a `Explorer` enemy. it cannot shoot yet.

---
+++ static/js/modules/ai/characters/explorer.js
@@ -0,0 +1,50 @@
+define(['underscore', 'gamejs', 'modules/ai/characters/enemy', 'modules/globals', 'modules/helpers/utils', 'gamejs/utils/math', 'gamejs/utils/vectors'], function(_, gamejs, enemy, globals, utils, $m, $v) {
+    /*
+     * Explorer ship.
+     * Really dumb ships that fire just once in a while and move straight
+     * forward in a semi random direction.
+     */
+    var Explorer = function() {
+        Explorer.superConstructor.apply(this, arguments);
+
+        // internal
+        var orientationVariance = Math.random() * Math.PI / 32 * (Math.random() < 0.5 ? 1 : -1);  // ugh
+        this.orientation = 3 * Math.PI / 2 + orientationVariance;
+
+        // basics...
+        this.image = gamejs.transform.rotate(gamejs.image.load(globals.enemies.images.explorer), $m.degrees(this.orientation + Math.PI));
+        this.rect  = new gamejs.Rect([0, 0], this.image.getSize());
+        this.mask  = gamejs.mask.fromSurface(this.image);
+
+        // this ones might vary according to the level, but are common to every
+        // enemy sprite.
+        this.speed    = utils.randomBetween(0.9, 1, false) * 7;
+        this.life     = 3;
+
+        // firing
+        this.fireRate = 1000 / globals.game.fps * 120;
+    };
+    gamejs.utils.objects.extend(Explorer, enemy.Enemy);
+
+    Explorer.prototype.setInitialPosition = function() {
+        enemy.Enemy.prototype.setInitialPosition.call(this);
+        var halfScreen = globals.game.screenSize[1] / 2;
+        if (this.orientation > 3 * Math.PI / 2 && this.rect.top < halfScreen)
+            this.rect.top += halfScreen;
+        else if (this.orientation < 3 * Math.PI / 2 && this.rect.top > halfScreen)
+            this.rect.top -= halfScreen;
+        return;
+    };
+
+    Explorer.prototype.update = function(msDuration) {
+        this.rect.left += this.speed * Math.cos(this.orientation - Math.PI/2);
+        this.rect.top  += this.speed * Math.sin(this.orientation - Math.PI/2);
+        if (this.rect.left < -this.image.getSize()[0] || this.rect.top < -this.image.getSize()[1] || this.rect.top > globals.game.screenSize[1])
+            this.kill();
+    };
+
+
+    //
+    // return API
+    return { Explorer: Explorer };
+});
--- static/js/modules/ai/foes.js
+++ static/js/modules/ai/foes.js
@@ -1,16 +1,22 @@
-define(['modules/ai/characters/meteor'], function(meteor) {
-    /*
-     * This is basically a proxy module for all the enemies in the
-     * static/js/modules/ai/enemies/ lib
-     *
-     * In this module we also implement Enemy, the base class for all the
-     * monsters.
-     */
+define(
+    [
+        'modules/ai/characters/meteor',
+        'modules/ai/characters/explorer'
+    ],
+    function(meteor, explorer) {
+        /*
+         * This is basically a proxy module for all the enemies in the
+         * static/js/modules/ai/enemies/ lib
+         *
+         * In this module we also implement Enemy, the base class for all the
+         * monsters.
+         */
 
 
-    //
-    // Return API
-    return {
-        Meteor: meteor.Meteor
-    };
-});
+        //
+        // Return API
+        return {
+            Explorer: explorer.Explorer,
+            Meteor: meteor.Meteor
+        };
+    });
